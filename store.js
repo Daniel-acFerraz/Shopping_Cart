94f54d1004d8be3baed6a4fb443b6df78f4fc1ff
Daniel-acFerraz <daniel.albinocf@gmail.com>
2023-03-30 16:52:24 -0300

    Finished Project

--- store.js
+++ store.js
@@ -1,9 +1,12 @@
+//function to check if document is still loading or ready to use
 if (document.readyState == "loading") {
     document.addEventListener('DOMContentLoaded', ready)
 } else {
     ready()
 }
+//function to check if document is still loading or ready to use
 
+//every event listeners from all of the items that have already been loaded
 function ready(){
     var  removeCartItemButtons = document.getElementsByClassName('btn-danger')
     console.log(removeCartItemButtons)
@@ -17,6 +20,25 @@ function ready(){
         var input = quantityInputs[i]
         input.addEventListener('change', quantityChanged)
     }
+
+    var addToCartButtons = document.getElementsByClassName('shop-item-button')
+    for(let i = 0; i < addToCartButtons.length; i++) {
+        let button = addToCartButtons[i]
+        button.addEventListener('click', addToCartClicked)
+    }
+
+    document.getElementsByClassName('btn-purchase')[0].addEventListener('click', purchasedClicked)
+}
+//every event listeners from all of the items that have already been loaded
+
+//then the whe have each function to each event in each button/input
+function purchasedClicked() {
+    alert('Thank you for your purchase!')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    while (cartItems.hasChildNodes()) {
+        cartItems.removeChild(cartItems.firstChild)
+    }
+    updateCartTotal()
 }
 
 function removeCartItem(event){
@@ -31,6 +53,45 @@ function quantityChanged(event) {
         input.value = 1
     }
     updateCartTotal()
+}
+
+function addToCartClicked(event) {
+    var button = event.target
+    var shopItem = button.parentElement.parentElement
+    var title = shopItem.getElementsByClassName('shop-item-title')[0].innerText
+    var price = shopItem.getElementsByClassName('shop-item-price')[0].innerText
+    var imageSrc = shopItem.getElementsByClassName('shop-item-image')[0].src 
+    addItemToCart(title, price, imageSrc)
+    updateCartTotal()
+}
+function addItemToCart(title, price, imageSrc) {
+    var cartRow = document.createElement('div')
+    cartRow.classList.add('cart-row')
+    var cartItems = document.getElementsByClassName('cart-items')[0]
+    var cartItemsNames = cartItems.getElementsByClassName('cart-item-title')
+    for (let i = 0; i < cartItemsNames.length; i++) {
+        if (cartItemsNames[i].innerText == title) {
+            alert('Item already in the cart!')
+            return
+        }
+    }
+    cartRowContents = `
+        <div class="cart-item cart-column">
+            <img class="cart-item-image" src="${imageSrc}" width="100" height="100">
+            <span class="cart-item-title">${title}</span>
+        </div>
+        <span class="cart-price cart-column">${price}</span>
+        <div class="cart-quantity cart-column">
+            <input class="cart-quantity-input" type="number" value="1">
+            <button class="btn btn-danger" type="button">REMOVE</button>
+        </div>
+    `
+    cartRow.innerHTML = cartRowContents
+    cartItems.append(cartRow)
+    //be sure to add every event listener to any new Element added to the DOM
+    cartRow.getElementsByClassName('btn-danger')[0].addEventListener('click', removeCartItem)
+    cartRow.getElementsByClassName('cart-quantity-input')[0].addEventListener('change', quantityChanged)
+    //be sure to add every event listener to any new Element added to the DOM
 }
 
 
