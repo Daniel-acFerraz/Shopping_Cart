b2527bd617f1a418611aac86f9cf6d586c8a900b
Daniel-acFerraz <daniel.albinocf@gmail.com>
2023-03-30 15:58:31 -0300

    Update store.js

--- store.js
+++ store.js
@@ -1,13 +1,39 @@
-var  removeCartItemButtons = document.getElementsByClassName('btn-danger')
-console.log(removeCartItemButtons)
-for(let i = 0; i < removeCartItemButtons.length; i++) {
-    let button = removeCartItemButtons[i]
-    button.addEventListener('click', function(event){
-        var buttonClicked = event.target
-        buttonClicked.parentElement.parentElement.remove()
-        updateCartTotal()
-    })
+if (document.readyState == "loading") {
+    document.addEventListener('DOMContentLoaded', ready)
+} else {
+    ready()
 }
+
+function ready(){
+    var  removeCartItemButtons = document.getElementsByClassName('btn-danger')
+    console.log(removeCartItemButtons)
+    for(let i = 0; i < removeCartItemButtons.length; i++) {
+     let button = removeCartItemButtons[i]
+        button.addEventListener('click', removeCartItem)
+    }
+
+    var quantityInputs = document.getElementsByClassName('cart-quantity-input')
+    for(let i = 0; i < quantityInputs.length; i++) {
+        var input = quantityInputs[i]
+        input.addEventListener('change', quantityChanged)
+    }
+}
+
+function removeCartItem(event){
+    var buttonClicked = event.target
+    buttonClicked.parentElement.parentElement.remove()
+    updateCartTotal()
+}
+
+function quantityChanged(event) {
+    let input = event.target
+    if(isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+}
+
+
 
 function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName("cart-items")[0]
@@ -21,6 +47,7 @@ function updateCartTotal() {
         let quantity = quantityElement.value 
         total += (price * quantity)
     }
+    total = Math.round(total * 100) / 100
     totalElement = document.getElementsByClassName('cart-total-price')[0]
     totalElement.innerText = '$' + total
 }
